635eb70ac5ac3f6299a3cf4e169f744a5f0865b1
Pulkit Garg <pulkitgargbnl@gmail.com>
2025-10-04 09:38:24 +0530

    chore: refactor code

---
+++ src/assets/js/footer.js
@@ -0,0 +1,13 @@
+document.getElementById("current-year").textContent = new Date().getFullYear();
+
+function accountRoute() {
+  const currentUser =
+    sessionStorage.getItem("currentUser") ||
+    localStorage.getItem("currentUser");
+
+  if (currentUser) {
+    window.location.href = "./account.html";
+  } else {
+    window.location.href = "./login.html";
+  }
+}
